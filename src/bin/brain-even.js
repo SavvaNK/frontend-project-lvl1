88649a540a53443e0be02a911abec3d72d4113b1
Savva Kirichenko <savvakn@gmail.com>
2020-10-05 17:22:52 +0300

    step 5 alfa version

--- src/bin/brain-even.js
+++ src/bin/brain-even.js
@@ -1,10 +1,29 @@
+import readlineSync from 'readline-sync';
+
 export const generateRandomInt = (min, max) => (
   Math.floor(Math.random() * (max - min + 1) + min)
 );
 
 export const isEven = (num) => num % 2 === 0;
 
-const number = generateRandomInt(0, 100);
+const numOfTries = 3;
 
-console.log(`Answer "yes" if the number is even, otherwise answer "no".
-Question: ${number}`);
+for (let i = numOfTries; i > 0; i -= 1) {
+  const number = generateRandomInt(0, 100);
+
+  const rightAnswer = isEven(number) ? 'yes' : 'no';
+
+  const userAnswer = readlineSync.question(`Answer "yes" if the number is even, otherwise answer "no".\nQuestion: ${number}\nYour answer: `);
+
+  if (userAnswer === rightAnswer) {
+    console.log('Correct!');
+  } else {
+    console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${rightAnswer}'.`);
+    console.log("Let's try again, $userName!");
+    break;
+  }
+
+  if (i === 1) {
+    console.log('Congratulations, $userName!');
+  }
+}
